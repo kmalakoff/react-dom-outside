0b80e20e12058b0de0d075491a953801d8fbf8f0
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 23:41:50 -0800

    Migrate to Biome and update development tooling

--- test/lib/polyfills.cjs
+++ test/lib/polyfills.cjs
@@ -1,8 +1,8 @@
 (function ensureDom() {
   if (typeof window !== 'undefined') return; // running in the browser
 
-  var jsdom = require('jsdom');
-  var doc = new jsdom.jsdom('<!doctype html><html><body></body></html>');
+  const jsdom = require('jsdom');
+  const doc = new jsdom.jsdom('<!doctype html><html><body></body></html>');
   global.document = doc;
   global.window = doc.defaultView;
   global.navigator = doc.defaultView.navigator;
