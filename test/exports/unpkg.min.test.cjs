abc344cdf725ff772f391a98e08e632488c5c73b
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 23:41:50 -0800

    Migrate to Biome and update development tooling

--- test/exports/unpkg.min.test.cjs
+++ test/exports/unpkg.min.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.min.js');
 
-describe('exports react-dom-outside/dist/umd/react-dom-outside.min.js', function () {
-  it('defaults', function () {
+describe('exports react-dom-outside/dist/umd/react-dom-outside.min.js', () => {
+  it('defaults', () => {
     assert.equal(typeof Active, 'function');
     assert.equal(typeof ActiveBoundary, 'function');
   });
