43afdc7e03cf494539ba8f23ed1bc5dd726832c9
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 20:23:06 -0800

    Upgrade modules

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.min.js');
+const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.cjs');
 
-describe('exports react-dom-outside/dist/umd/react-dom-outside.js', () => {
+describe('exports react-dom-outside/dist/umd/react-dom-outside.cjs', () => {
   it('defaults', () => {
     assert.equal(typeof Active, 'function');
     assert.equal(typeof ActiveBoundary, 'function');
