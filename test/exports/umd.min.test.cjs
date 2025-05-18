bd8ceef9a766da579ee1026a6cdf9a7589a8cb8e
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 17:47:58 -0700

    Update package exports and type definitions

--- test/exports/umd.min.test.cjs
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.min.cjs');
-
-describe('exports react-dom-outside/dist/umd/react-dom-outside.min.cjs', () => {
-  it('defaults', () => {
-    assert.equal(typeof Active, 'function');
-    assert.equal(typeof ActiveBoundary, 'function');
-  });
-});
