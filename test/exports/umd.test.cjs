5448267f688baae9af327b3f68313e01c8f2d569
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 17:47:58 -0700

    Update package exports and type definitions

--- test/exports/umd.test.cjs
+++ test/exports/umd.test.cjs
@@ -1,7 +1,15 @@
 const assert = require('assert');
-const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.cjs');
 
-describe('exports react-dom-outside/dist/umd/react-dom-outside.cjs', () => {
+let umd = null;
+try {
+  umd = require('react-dom-outside/umd');
+} catch (_) {
+  umd = require('react-dom-outside/dist/umd/react-dom-outside.cjs');
+}
+const reactDomOutside = typeof window !== 'undefined' ? window.reactDomOutside : umd.default || umd;
+const { Active, ActiveBoundary } = reactDomOutside;
+
+describe('exports umd', () => {
   it('defaults', () => {
     assert.equal(typeof Active, 'function');
     assert.equal(typeof ActiveBoundary, 'function');
