a6e0a9d99683cc89566b322325f6f589c12e0a6b
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:31:47 -0700

    Comprehensive types

--- src/Active.ts
+++ src/Active.ts
@@ -2,7 +2,9 @@ import type { Attributes, ReactNode } from 'react';
 import { Children, cloneElement, createElement, Fragment, isValidElement, useRef, useState } from 'react';
 import { useEvent } from 'react-dom-event';
 
-export default function Active({ children }) {
+import type { ActiveProps } from './types.ts';
+
+export default function Active({ children }: ActiveProps) {
   const state = useState<boolean>(false);
   const isActive = state[0];
   const setIsActive = state[1];
