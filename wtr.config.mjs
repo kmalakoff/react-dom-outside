daf9ec51d894a91c2ac2f02f81c4186ba7b88878
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 20:50:16 -0700

    Update CI workflow, dev scripts, and local configurations

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -2,7 +2,7 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 import createConfig from 'tsds-web-test-runner/createConfig.mjs';
 
 export default createConfig({
-  port: 8008,
+  port: 9009,
   plugins: [
     importMapsPlugin({
       inject: {
